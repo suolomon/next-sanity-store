9ca0401649bded802d1e22f49f46d0e6c3da2e7d
Solomon Talemwa <adamtblack96@gmail.com>
2022-05-22 14:41:43 +0300

    add navbar and footer component

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer-container">
+      <p> &copy; {new Date().getFullYear()} Demo Store. All rights reserved.</p>
+      <p>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
